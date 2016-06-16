46b3eeb78bc525d85d67d1f661a346bb471664e5
Tjaden Hess <tjade273@gmail.com>
2016-06-16 00:34:36 -0400

    Beautify

--- contracts/ProofLib.sol
+++ contracts/ProofLib.sol
@@ -1,81 +1,78 @@
-library ProofLib{
+library ProofLib {
 
-  struct Proof{
-    address defender;
-    address challenger;
+    struct Proof {
+        address defender;
+        address challenger;
 
-    //uint deposit;
+        //uint deposit;
 
-    bytes32 lVal;
-    bytes32 rVal;
-    uint lIndex;
-    uint rIndex;
+        bytes32 lVal;
+        bytes32 rVal;
+        uint lIndex;
+        uint rIndex;
 
-    bytes32 currentVal;
+        bytes32 currentVal;
 
-    //TODO: Implement non-participation resilliancy
-  }
-
-
-  function newChallenge(Proof storage self, address _defender, address _challenger, bytes32 seed, bytes32 result, uint difficulty){
-    self.defender = _defender;
-    self.challenger = _challenger;
-
-    self.lVal = seed;
-    self.rVal = result;
-
-    self.lIndex = 0;
-    self.rIndex = difficulty;
-
-
-  }
-
-  function challenge(Proof storage self, bool correct){
-    if(self.currentVal == 0 || msg.sender != self.challenger) throw;
-    if(correct){
-      self.lIndex = (self.lIndex + self.rIndex)/2;
-      self.lVal = self.currentVal;
-    }
-    else{
-      self.rIndex = (self.lIndex + self.rIndex)/2;
-      self.rVal = self.currentVal;
+        //TODO: Implement non-participation resilliancy
     }
 
-    self.currentVal = 0;
-  }
 
-  function respond(Proof storage self, bytes32 hash){
+    function newChallenge(Proof storage self, address _defender, address _challenger, bytes32 seed, bytes32 result, uint difficulty) {
+        self.defender = _defender;
+        self.challenger = _challenger;
 
-    if(self.currentVal != 0 || msg.sender != self.defender) throw;
-    self.currentVal = hash;
-  }
+        self.lVal = seed;
+        self.rVal = result;
 
-  function finalize(Proof storage self) returns (bool confirmed){ //returns true if challenge successfully disproves proposal
+        self.lIndex = 0;
+        self.rIndex = difficulty;
 
-    if(self.rIndex - self.lIndex <= 3){
-      bytes32 hash = self.lVal;
 
-      for(uint i; i < self.rIndex - self.lIndex; i++){
-        hash = sha3(hash);
-      }
-
-      if(hash == self.rVal){
-        return false;
-      }
-      else{
-        return true;
-      }
     }
-    else throw;
-  }
 
-/*
- function getProof(uint id) constant returns(uint,uint,bytes32,bytes32,bytes32){
-     return (proofs[id].lIndex,proofs[id].rIndex,proofs[id].lVal,proofs[id].currentVal, proofs[id].rVal);
- }
+    function challenge(Proof storage self, bool correct) {
+        if (self.currentVal == 0 || msg.sender != self.challenger) throw;
+        if (correct) {
+            self.lIndex = (self.lIndex + self.rIndex) / 2;
+            self.lVal = self.currentVal;
+        } else {
+            self.rIndex = (self.lIndex + self.rIndex) / 2;
+            self.rVal = self.currentVal;
+        }
 
- function sha(bytes32 seed) constant returns(bytes32){
-     return sha3(seed);
- }
-*/
+        self.currentVal = 0;
+    }
+
+    function respond(Proof storage self, bytes32 hash) {
+
+        if (self.currentVal != 0 || msg.sender != self.defender) throw;
+        self.currentVal = hash;
+    }
+
+    function finalize(Proof storage self) returns(bool confirmed) { //returns true if challenge successfully disproves proposal
+
+        if (self.rIndex - self.lIndex <= 3) {
+            bytes32 hash = self.lVal;
+
+            for (uint i; i < self.rIndex - self.lIndex; i++) {
+                hash = sha3(hash);
+            }
+
+            if (hash == self.rVal) {
+                return false;
+            } else {
+                return true;
+            }
+        } else throw;
+    }
+
+    /*
+     function getProof(uint id) constant returns(uint,uint,bytes32,bytes32,bytes32){
+         return (proofs[id].lIndex,proofs[id].rIndex,proofs[id].lVal,proofs[id].currentVal, proofs[id].rVal);
+     }
+
+     function sha(bytes32 seed) constant returns(bytes32){
+         return sha3(seed);
+     }
+    */
 }
--- contracts/RanDAOPlus.sol
+++ contracts/RanDAOPlus.sol
@@ -1,90 +1,90 @@
 import "ProofLib.sol";
 
-contract RanDAOPlus{
-  using ProofLib for ProofLib.Proof;
+contract RanDAOPlus {
+    using ProofLib
+    for ProofLib.Proof;
 
 
-  uint constant timeout = 10;
+    uint constant timeout = 10;
 
-  mapping(uint => bytes32) finalizedRandomNumbers; //Maps block numbers to random numbers: One random number is generated per block
+    mapping(uint => bytes32) finalizedRandomNumbers; //Maps block numbers to random numbers: One random number is generated per block
 
-  ProofLib proofLib;
+    ProofLib proofLib;
 
-  struct Proposal {
-    uint block;
-    bytes32 proposal;
-    uint deposit;
-    address depositor;
-    mapping(address => ProofLib.Proof) challenges;
-    bool disproven;
-  }
-
-  struct PendingBlock {
-    uint blockNumber;
-    uint difficulty;
-    uint depositLimit;
-    mapping(bytes32 => uint) depositTotals;
-    mapping(address => Proposal) proposals;
-
-    bytes32 topProposal;
-    uint timer;
-  }
-
-  mapping(uint => PendingBlock) pending;
-
-  function RanDAOPlus(address proof){
-    proofLib = ProofLib(proof);
-  }
-
-  function submitProposal(uint block, bytes32 proposal){
-    if(finalizedRandomNumbers[block] != 0 || pending[block].blockNumber == 0) throw;
-
-    Proposal prop = pending[block].proposals[msg.sender];
-    prop.block = block;
-    prop.proposal = proposal;
-    prop.deposit = msg.value;
-    prop.depositor = msg.sender;
-
-    pending[block].depositTotals[proposal] += msg.value;
-
-    if(pending[block].depositTotals[proposal] > pending[block].depositTotals[pending[block].topProposal] && pending[block].depositTotals[proposal] > pending[block].depositLimit){
-      pending[block].topProposal = proposal;
-      pending[block].timer = block + timeout;
+    struct Proposal {
+        uint block;
+        bytes32 proposal;
+        uint deposit;
+        address depositor;
+        mapping(address => ProofLib.Proof) challenges;
+        bool disproven;
     }
-  }
 
-function newChallenge(uint blockNum, address defender){
-   ProofLib.Proof proof = pending[blockNum].proposals[defender].challenges[msg.sender];
+    struct PendingBlock {
+        uint blockNumber;
+        uint difficulty;
+        uint depositLimit;
+        mapping(bytes32 => uint) depositTotals;
+        mapping(address => Proposal) proposals;
 
-   proof.newChallenge(defender,msg.sender, block.blockhash(blockNum), pending[blockNum].proposals[defender].proposal, pending[blockNum].difficulty);
-   pending[blockNum].depositTotals[pending[blockNum].proposals[defender].proposal] -= pending[blockNum].proposals[defender].deposit;
- }
-
-  function challenge(uint block, address defender, bool correct){
-    pending[block].proposals[defender].challenges[msg.sender].challenge(correct);
-  }
-
-  function respond(uint block, address challenger, bytes32 response){
-    pending[block].proposals[msg.sender].challenges[challenger].respond(response);
-  }
-
-  function finalize(uint block, address defender, address challenger){
-    bool challengeSuccessful = pending[block].proposals[defender].challenges[challenger].finalize();
-    delete pending[block].proposals[defender].challenges[challenger];
-
-    if(challengeSuccessful){
-      challenger.send(pending[block].proposals[defender].deposit * 2);
-      pending[block].proposals[defender].disproven = true;
+        bytes32 topProposal;
+        uint timer;
     }
-    else{
-      defender.send(pending[block].proposals[defender].deposit);
-    }
-  }
 
-  function finalizeNumber(uint blockNum){
-    if(pending[blockNum].topProposal != 0 && pending[blockNum].timer <= block.number){
-      finalizedRandomNumbers[blockNum] = pending[blockNum].topProposal;
-      delete pending[blockNum];
+    mapping(uint => PendingBlock) pending;
+
+    function RanDAOPlus(address proof) {
+        proofLib = ProofLib(proof);
     }
-  }
+
+    function submitProposal(uint block, bytes32 proposal) {
+        if (finalizedRandomNumbers[block] != 0 || pending[block].blockNumber == 0) throw;
+
+        Proposal prop = pending[block].proposals[msg.sender];
+        prop.block = block;
+        prop.proposal = proposal;
+        prop.deposit = msg.value;
+        prop.depositor = msg.sender;
+
+        pending[block].depositTotals[proposal] += msg.value;
+
+        if (pending[block].depositTotals[proposal] > pending[block].depositTotals[pending[block].topProposal] && pending[block].depositTotals[proposal] > pending[block].depositLimit) {
+            pending[block].topProposal = proposal;
+            pending[block].timer = block + timeout;
+        }
+    }
+
+    function newChallenge(uint blockNum, address defender) {
+        ProofLib.Proof proof = pending[blockNum].proposals[defender].challenges[msg.sender];
+
+        proof.newChallenge(defender, msg.sender, block.blockhash(blockNum), pending[blockNum].proposals[defender].proposal, pending[blockNum].difficulty);
+        pending[blockNum].depositTotals[pending[blockNum].proposals[defender].proposal] -= pending[blockNum].proposals[defender].deposit;
+    }
+
+    function challenge(uint block, address defender, bool correct) {
+        pending[block].proposals[defender].challenges[msg.sender].challenge(correct);
+    }
+
+    function respond(uint block, address challenger, bytes32 response) {
+        pending[block].proposals[msg.sender].challenges[challenger].respond(response);
+    }
+
+    function finalize(uint block, address defender, address challenger) {
+        bool challengeSuccessful = pending[block].proposals[defender].challenges[challenger].finalize();
+        delete pending[block].proposals[defender].challenges[challenger];
+
+        if (challengeSuccessful) {
+            challenger.send(pending[block].proposals[defender].deposit * 2);
+            pending[block].proposals[defender].disproven = true;
+        } else {
+            defender.send(pending[block].proposals[defender].deposit);
+        }
+    }
+
+    function finalizeNumber(uint blockNum) {
+        if (pending[blockNum].topProposal != 0 && pending[blockNum].timer <= block.number) {
+            finalizedRandomNumbers[blockNum] = pending[blockNum].topProposal;
+            delete pending[blockNum];
+        }
+    }
 }
