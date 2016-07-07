a0e7eae91468f54a59e4e6257d8ec3511542c773
Tjaden Hess <tjade273@gmail.com>
2016-07-07 13:36:09 -0400

    Add hackathon work

--- contracts/ProofLib.sol
+++ contracts/ProofLib.sol
@@ -1,26 +1,26 @@
 library ProofLib {
 
     struct Proof {
-        address defender;
-        address challenger;
+        address defender; //Not guaranteed to be initiallized
+        address challenger; // Should hopefully be initialized. Onus is on the challenger to disprove
 
-        bytes32 lVal;
-        bytes32 rVal;
-        uint lIndex;
-        uint rIndex;
+        uint lVal; //The left hash value of the current frame
+        uint rVal; //Right value
+        uint lIndex; //The index in the chain that the sliding window starts on-- should only ever move right
+        uint rIndex; //The last index that we are examining -- only should move left
 
-        uint roundTime;
-        uint lastRound;
+        uint roundTime; //Time before "call timeout" should be allowed (in blocks)
+        uint lastRound; //Last time a person acted (i.e. start time for current timeout timer)
 
-        bytes32 currentVal;
+        uint currentVal; //The value under consideration. 0 indicates defender's turn, any other value indicates challenger's turn
 
         //TODO: Implement non-participation resilliancy
     }
 
 
-    function newChallenge(Proof storage self, address _defender, address _challenger, bytes32 seed, bytes32 result, uint difficulty, uint time) {
-        self.defender = _defender;
-        self.challenger = _challenger;
+    function newChallenge(Proof storage self, address _challenger, uint seed, uint result, uint difficulty, uint time) {
+        //Normally, defender would be assigned here. TODO: make sure we handle 0 defender properly
+        self.challenger = _challenger; // assign challenger
 
         self.lVal = seed;
         self.rVal = result;
@@ -28,50 +28,55 @@ library ProofLib {
         self.lIndex = 0;
         self.rIndex = difficulty;
 
-        self.roundTime = time;
-        self.lastRound = block.number;
+        self.roundTime = time; // How long this round can last -- TODO(ish): make sure 0 is handled properly
+        self.lastRound = block.number; // Start timer. Challenger initiates, so defender has to respond within roundTime
 
 
     }
 
     function challenge(Proof storage self, bool correct) {
-        if (self.currentVal == 0 || msg.sender != self.challenger) throw;
+        if (self.currentVal == 0 ||  //Defender hasn't responded yet
+            msg.sender != self.challenger) throw;  //Someone's impersonating the challenger TODO: don't do this check twice
         if (correct) {
-            self.lIndex = (self.lIndex + self.rIndex) / 2;
+            self.lIndex = (self.lIndex + self.rIndex) / 2; // If correct, slide window down-chain TODO: understand rounding behavior
             self.lVal = self.currentVal;
         } else {
-            self.rIndex = (self.lIndex + self.rIndex) / 2;
+            self.rIndex = (self.lIndex + self.rIndex) / 2;  // If incorrect, move window upstream
             self.rVal = self.currentVal;
         }
 
-        self.currentVal = 0;
+        self.currentVal = 0; // Hand control back to defender
+        //TODO: reset timer
     }
 
-    function respond(Proof storage self, bytes32 hash) {
-        if (self.currentVal != 0 || msg.sender != self.defender) throw;
-        self.currentVal = hash;
+    function respond(Proof storage self, uint hash) {
+        if (self.currentVal != 0 ||  //It's the challenger's turn
+            msg.sender != self.defender) throw; // No impersonating the defender
+        self.currentVal = hash; // Assert that currentVal == hash
+
+        //TODO: reset timer
     }
 
     function finalize(Proof storage self) returns(bool confirmed) { //returns true if challenge successfully disproves proposal
 
-        if (self.rIndex - self.lIndex <= 3) {
-            bytes32 hash = self.lVal;
+        if (self.rIndex - self.lIndex <= 3) { // Only finalize if we have to do less than 4 hashes (pretty reasonable)
+            bytes32 hash = bytes32(self.lVal); // TODO: Worry about types.... Does casting change the sha3 value?
 
-            for (uint i; i < self.rIndex - self.lIndex; i++) {
-                hash = sha3(hash);
+            for (uint i; i < self.rIndex - self.lIndex; i++) { //Finish off the computation
+                hash = sha3(hash); //Worry about types....
             }
 
-            if (hash == self.rVal) {
+            if (hash == bytes32(self.rVal)) {  // If the hash checks out, the challenge failed
                 return false;
-            } else {
+            } else {                           // Else the challenger wins
                 return true;
             }
-        } else throw;
+        } else throw;   //Don't go through with the computation if it's longer than 3 hashes
     }
 
 
-     function getProof(Proof storage self) constant returns(uint,uint,bytes32,bytes32,bytes32){
-         return (self.lIndex, self.rIndex, self.lVal, self.currentVal, self.rVal);
+     function getProof(Proof storage self) constant returns(uint,uint,uint,uint,uint){
+         return (self.lIndex, self.rIndex, self.lVal, self.currentVal, self.rVal);  //Generic getter for proofs
      }
 
 
--- contracts/RanDAOPlus.sol
+++ contracts/RanDAOPlus.sol
@@ -1,139 +1,173 @@
 import "./ProofLib.sol";
+contract RNG {
+  using ProofLib for ProofLib.Proof;
+  mapping(uint => uint) public randomNumbers; //Finalized random numbers. If non-zero, can be assumed final
+  mapping(uint => pendingBlock) pending;  //Pending numbers
 
-contract RanDAOPlus {
-    using ProofLib for ProofLib.Proof;
+  struct Deposit {
+    uint proposal;
+    int amount; //Negative if betting against proposal, positive if betting for
+  }
 
-    //EVENTS FOR DEBUGGING ONLY
+  struct pendingBlock{
+    mapping(uint => uint) proposals; //Maps proposed solutions to amount staked
+    mapping(address => Deposit) deposits; //Maps addresses to Deposits
+    mapping(uint => ProofLib.Proof[]) proofs; //Maps proposal to array of proofs
+    uint blockhash; //Seed for RNG
+    uint totalFunds; //Total balance collected for this block
+    uint depositLimit; //Limit above which proposals may be finalized -- new top proposal resets the limit
+    uint difficulty; //Number of hashes on seed
+    uint finalizeTime; // Block number at which top proposal becomes final;
+  }
 
+  uint public diff = 15000; //Start difficulty at 20, adjustment algo TBD
+  uint public constant fee = 1 finney; //Minimum fee to buy random number
+  uint public constant minDeposit = 1 ether;
 
+  //Debugging events
+  event DepositLimitChange(uint blockNum, uint limit);
+  event NewProposal(uint blockNum, uint proposal, uint deposit);
+  event NewChallenge(uint blockNum, uint proposal, address challenger, uint proofID);
+  event ChallengeAccepted(uint blockNum, uint proposal, address defender, uint proofID);
+  event FinalizeProof(uint blockNum, uint proposal, uint proofID, bool success);
+  event Victory(uint blockNum, uint proposal);
 
-    uint constant timeout = 10;
-    uint constant difficultyTarget = 6;
-    uint constant proofRoundTime = 5;
+  function buyNumber(uint blockNum){ //Increase security of number at block blockNum
+    pending[blockNum].depositLimit += msg.value*10; //@debug Increase min deposit: depositLimit + `uint msg.value` * 10 = `uint pending[blockNum].depositLimit`
+    pending[blockNum].totalFunds += msg.value;
+    DepositLimitChange(blockNum, pending[blockNum].depositLimit);
+  }
 
-    uint difficulty;
+  function initBlock(uint blockNum){
+    pending[blockNum].difficulty = diff;
+    pending[blockNum].blockhash = uint(block.blockhash(blockNum)); // TODO, make sure not 0
+  }
 
-    mapping(uint => bytes32) finalizedRandomNumbers; //Maps block numbers to random numbers: One random number is generated per block
+  function deposit(uint blockNum, uint proposal){
+    if(blockNum > block.number || proposal == 0) throw; //@warn Don't let people bet on bocks in the future
+    Deposit deposit = pending[blockNum].deposits[msg.sender]; // Get deposit for msg.sender
+    if(deposit.proposal != 0 && deposit.proposal != proposal) throw; //@warn If they've already submitted a proposal, throw. They can make more accounts if they want more than one proposal (which they shouldn't)
 
-    ProofLib proofLib;
+    initBlock(blockNum);
+    deposit.proposal = proposal; //@debug Set their proposal to the proposal (possibly overwrite with same value, a bit inefficient: `uint deposit.proposal`)
+    deposit.amount += int(msg.value); //Add to their deposit (TODO: check for overflows)
+    uint deposits = pending[blockNum].proposals[proposal]; // Store deposit total for later
+    pending[blockNum].proposals[proposal] += msg.value; //Add their deposit to total value backing proposal
+    pending[blockNum].totalFunds += msg.value; //Add deposit to total funds collected
+    if(!(deposits >= pending[blockNum].depositLimit) && pending[blockNum].proposals[proposal] > pending[blockNum].depositLimit){  //If this is the new top proposal
+      pending[blockNum].depositLimit = pending[blockNum].proposals[proposal];  //depositLimit increases
+      pending[blockNum].finalizeTime += 3; // add another 3 blocks to the finalization deadline TODO: migrate to global, possibly dynamic variable
+    }
+    NewProposal(blockNum, proposal, pending[blockNum].proposals[proposal]);
+  }
 
-    struct Proposal {
-        uint block;
-        bytes32 proposal;
-        uint deposit;
-        address depositor;
-        mapping(address => ProofLib.Proof) challenges;
-        bool disproven;
+  function newChallenge(uint blockNum, uint proposal){ //Create new challenge
+    if(msg.value < minDeposit) throw;  //@warn Don't allow deposits below min deposit: WHY?
+
+    ProofLib.Proof[] proofs = pending[blockNum].proofs[proposal];
+    proofs[proofs.length++].newChallenge(msg.sender, pending[blockNum].blockhash, proposal, diff, 20); // Create an new challenge with timeout 20. TODO: Dynamic timeouts?
+
+    Deposit deposit = pending[blockNum].deposits[msg.sender];
+    if(deposit.proposal != 0 && deposit.proposal != proposal) throw; //Don't let someone bet against more than one proposal TODO: Allow this
+    deposit.proposal = proposal; // Set sender's proposal
+    pending[blockNum].deposits[msg.sender].amount = -int(msg.value); //Set sender's deposit amout to negative to indicate nay vote
+    pending[blockNum].proposals[proposal] -= msg.value; //Subtract from total value backing proposal TODO: check for underflow
+
+    NewChallenge(blockNum, proposal, msg.sender, proofs.length -1);
+  }
+
+  function acceptChallenge(uint blockNum, uint proposal, uint proofIndex){
+    pendingBlock p = pending[blockNum];
+    if(p.deposits[msg.sender].proposal != proposal || //Don't allow defending on proposals that aren't yours
+    p.deposits[msg.sender].amount < -p.deposits[p.proofs[proposal][proofIndex].challenger].amount) throw;  //Require a vested interest in the defense
+
+    p.proofs[proposal][proofIndex].defender = msg.sender; // Set the sender as the defender TODO: don't overwrite defenders
+
+    ChallengeAccepted(blockNum, proposal, msg.sender, proofIndex);
+  }
+
+  //Pure pass-through functions
+  function challenge(uint blockNum, uint proposal, uint proofID, bool correct){ //Passthrough to ProofLib.challenge
+    ProofLib.Proof proof = pending[blockNum].proofs[proposal][proofID]; // Fetch proof
+    proof.challenge(correct);
+  }
+
+  function respond(uint blockNum, uint proposal, uint proofID, uint val){
+    ProofLib.Proof proof = pending[blockNum].proofs[proposal][proofID]; // Fetch proof
+    proof.respond(val);
+  }
+
+  function finalize(uint blockNum, uint proofIndex){ //Should only be called by challenger (TODO)
+
+    uint proposal = pending[blockNum].deposits[msg.sender].proposal; //TODO: make sure this is a valid proposal
+    address defender = pending[blockNum].proofs[proposal][proofIndex].defender;  //Get the defender
+    address challenger = pending[blockNum].proofs[proposal][proofIndex].challenger; //Get the challenger
+    int deposit;
+
+    if(pending[blockNum].proofs[proposal][proofIndex].finalize()){ //Check if the challenge was successful
+      deposit = pending[blockNum].deposits[defender].amount; //Get the deposit put down by the defender
+      pending[blockNum].deposits[defender].amount = 0; //Defender loses, their deposit is taken
+      pending[blockNum].deposits[challenger].amount -= deposit; //Make the deposit **More negative** TODO: Make sure that msg.sender really equals challenger
+      FinalizeProof(blockNum, proposal, proofIndex, true);
+    }
+    else { //The defender wins
+      deposit = pending[blockNum].deposits[challenger].amount;   //Get deposit by challenger
+      pending[blockNum].deposits[challenger].amount = 0;    //challenger loses their deposit
+      pending[blockNum].deposits[defender].amount -= deposit; // defender gets the challenger's deposit. (negative because challenger has negative deposit)
+      FinalizeProof(blockNum, proposal, proofIndex, false);
     }
 
-    struct PendingBlock {
-        uint blockNumber;
-        uint difficulty;
-        uint depositLimit;
-        mapping(bytes32 => uint) depositTotals;
-        mapping(address => Proposal) proposals;
-        mapping(bytes32 => uint) submissionTimes;
+  }
 
-        bytes32 topProposal;
-        uint timer;
+  function declareVictor(uint blockNum, uint proposal){ //Finalize number. Once this is called, there should be no going back < TODO!!!!
+    if(randomNumbers[blockNum] != 0 ||    // If there's already a number decided, throw. TODO: perform check upstream as well
+        pending[blockNum].finalizeTime > block.number) throw;  // If the finalize time hasn't passed, don't finalize
+    if(pending[blockNum].proposals[proposal] >= pending[blockNum].depositLimit){ // If the proposal deposits have passed the limit, it is finalized. TODO: make it stay above limin for time T
+      randomNumbers[blockNum] = proposal; // Moment of truth....
+      Victory(blockNum, proposal);
     }
+    //consider throwing
+  }
 
-    mapping(uint => PendingBlock) pending;
-
-    function RanDAOPlus(address proof) {
-        proofLib = ProofLib(proof);
+  function claimReward(uint blockNum){ //This needs to work, or all the game theory breaks down...
+    uint proposal = pending[blockNum].deposits[msg.sender].proposal; //Fetch the user's proposal
+    if(randomNumbers[blockNum] == proposal && //Only if they bet on the right thing
+    pending[blockNum].deposits[msg.sender].amount > 0){ // And they bet for it, not against TODO: what happens to the loser's deposits?
+      int amount = pending[blockNum].deposits[msg.sender].amount; // The amount they bet
+      pending[blockNum].deposits[msg.sender].amount = 0; // Zero out deposit to prevent recursive call attacks
+      if(!msg.sender.send((pending[blockNum].totalFunds * uint(amount))/pending[blockNum].proposals[proposal])) throw; // Send Total ammount collected for block * percent of winning bets the sender holds
     }
+  }
 
-    function submitProposal(uint blockNum, bytes32 proposal) {
-        if (finalizedRandomNumbers[blockNum] != 0 || pending[blockNum].blockNumber == 0) throw;
+  //Getters:
+  function getDeposit(uint blockNum, address addr) constant returns(uint proposal, int amount){ //Gets the deposit for a given account at blockNum
+    Deposit dep = pending[blockNum].deposits[addr];
+    return(dep.proposal, dep.amount);
+  }
 
-        Proposal prop = pending[blockNum].proposals[msg.sender];
-        prop.block = blockNum;
-        prop.proposal = proposal;
-        prop.deposit = msg.value;
-        prop.depositor = msg.sender;
+  function getStake(uint blockNum, uint proposal) constant returns (uint){ //Gets the net stake on proposal (indicates finality)
+    return pending[blockNum].proposals[proposal]; //Net stake on proposal
+  }
 
-        pending[blockNum].depositTotals[proposal] += msg.value;
+  function getPendingBlock(uint blockNum) constant returns (uint[5]){ //Returns all statically enumerable info about pending block
+    pendingBlock p = pending[blockNum];
+    return [p.blockhash, p.totalFunds, p.depositLimit, diff, p.finalizeTime];
+  }
 
-        if (pending[blockNum].depositTotals[proposal] > pending[blockNum].depositTotals[pending[blockNum].topProposal] && pending[blockNum].depositTotals[proposal] > pending[blockNum].depositLimit) {
-            pending[blockNum].topProposal = proposal;
-            pending[blockNum].timer = blockNum + timeout;
-        }
+  function getProof(uint blockNum, uint proposal, uint proofID) constant returns(address[2], uint[7]){
+    ProofLib.Proof proof = pending[blockNum].proofs[proposal][proofID];
 
-        if (pending[blockNum].submissionTimes[proposal] == 0) {
-            pending[blockNum].submissionTimes[proposal] = block.number;
-        }
+    return ([proof.defender, proof.challenger], [proof.lVal, proof.rVal, proof.lIndex, proof.rIndex, proof.roundTime, proof.lastRound, proof.currentVal]);
+  }
+
+
+
+  //Convinience Method for testing miner
+  function sha(uint blockNum, uint diff) constant returns (uint){ // TODO: Make sure this returns the correct result
+    bytes32 temp = block.blockhash(blockNum);
+    for(uint i = 0; i< diff; i++){
+      temp = sha3(temp);
     }
-
-    function newChallenge(uint blockNum, address defender) {
-        ProofLib.Proof proof = pending[blockNum].proposals[defender].challenges[msg.sender];
-
-        proof.newChallenge(defender, msg.sender, block.blockhash(blockNum), pending[blockNum].proposals[defender].proposal, pending[blockNum].difficulty, proofRoundTime);
-        pending[blockNum].depositTotals[pending[blockNum].proposals[defender].proposal] -= pending[blockNum].proposals[defender].deposit;
-    }
-
-    function challenge(uint block, address defender, bool correct) {
-        pending[block].proposals[defender].challenges[msg.sender].challenge(correct);
-    }
-
-    function respond(uint block, address challenger, bytes32 response) {
-        pending[block].proposals[msg.sender].challenges[challenger].respond(response);
-    }
-
-    function finalize(uint blockNum, address defender, address challenger) {
-        ProofLib.Proof proof = pending[blockNum].proposals[defender].challenges[challenger];
-        bool challengeSuccessful = proof.finalize();
-        distributeRewards(proof, challengeSuccessful, blockNum);
-        delete pending[blockNum].proposals[defender].challenges[challenger];
-    }
-
-    function distributeRewards(ProofLib.Proof proof, bool challengeSuccessful, uint blockNum) private { //Make sure loopback attacks not possible
-        if (challengeSuccessful) {
-            proof.challenger.send(pending[blockNum].proposals[proof.defender].deposit * 2);
-            pending[blockNum].proposals[proof.defender].disproven = true;
-        } else {
-            proof.defender.send(pending[blockNum].proposals[proof.defender].deposit);
-        }
-    }
-
-    function challengeTimeout(uint blockNum, bool isDefender, address opponent) {
-
-
-        if (isDefender) {
-          ProofLib.Proof proof = pending[blockNum].proposals[msg.sender].challenges[opponent];
-        } else {
-          proof = pending[blockNum].proposals[opponent].challenges[msg.sender];
-        }
-
-        if (proof.roundTime + proof.lastRound < block.number) {
-            if (proof.currentVal == 0) {
-                distributeRewards(proof, true, blockNum);
-            } else {
-                distributeRewards(proof, false, blockNum);
-            }
-        }
-    }
-
-    function finalizeNumber(uint blockNum) {
-        if (pending[blockNum].topProposal != 0 && pending[blockNum].timer <= block.number) {
-            finalizedRandomNumbers[blockNum] = pending[blockNum].topProposal;
-            adjustDifficulty(blockNum - pending[blockNum].submissionTimes[pending[blockNum].topProposal]);
-            delete pending[blockNum];
-
-        }
-    }
-
-    function adjustDifficulty(uint blocksToVerify) {
-        difficulty = difficultyTarget / blocksToVerify * difficulty; //TODO: Simulate network difficulty adjustment -- optimise algo
-    }
-
-    function sha(bytes32 seed) constant returns(bytes32){
-        return sha3(seed);
-    }
-
-    function getProof(uint blockNum, address defender, address challenger) constant returns (uint,uint,bytes32,bytes32,bytes32){
-      return pending[blockNum].proposals[defender].challenges[challenger].getProof();
-    }
-
-
+    return uint(temp);
+  }
 }
